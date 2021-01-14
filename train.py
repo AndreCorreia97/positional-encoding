87de050d4b90c7900697b1ef85cc6f516006be96
AndreCorreia97 <36961062+AndreCorreia97@users.noreply.github.com>
2021-01-14 17:59:30 +0000

    Update train.py

--- train.py
+++ train.py
@@ -9,42 +9,28 @@ from fairseq.data import iterators
 from fairseq.trainer import Trainer
 from fairseq.meters import AverageMeter, StopwatchMeter
 
-"""
-Namespace(adam_betas='(0.9, 0.999)', adam_eps=1e-08, adaptive_softmax_cutoff=None, adaptive_softmax_dropout=0, arch='transformer_wmt_en_de', attention_dropout=0.0, bucket_cap_mb=150, clip_norm=0.0, criterion='label_smoothed_cross_entropy', data=['data-bin/gigaWord'], ddp_backend='c10d', decoder_attention_heads=8, decoder_embed_dim=512, decoder_embed_path=None, decoder_ffn_embed_dim=2048, decoder_input_dim=512, decoder_layers=6, decoder_learned_pos=False, decoder_normalize_before=False, decoder_output_dim=512, device_id=0, distributed_backend='nccl', distributed_init_method=None, distributed_port=-1, distributed_rank=0, distributed_world_size=1, dropout=0.3, encoder_attention_heads=8, encoder_embed_dim=512, encoder_embed_path=None, encoder_ffn_embed_dim=2048, encoder_layers=6, encoder_learned_pos=False, encoder_normalize_before=False, fix_batches_to_gpus=False, fp16=False, fp16_init_scale=128, keep_interval_updates=-1, label_smoothing=0.1, left_pad_source='True', left_pad_target='False', log_format=None, log_interval=1000, lr=[0.001], lr_scheduler='inverse_sqrt', lr_shrink=0.1, max_epoch=100, max_sentences=None, max_sentences_valid=None, max_source_positions=1024, max_target_positions=1024, max_tokens=3584, max_update=0, min_loss_scale=0.0001, min_lr=1e-09, momentum=0.99, no_epoch_checkpoints=False, no_progress_bar=False, no_save=False, no_token_positional_embeddings=False, optimizer='adam', optimizer_overrides='{}', ordinary_sinpos=True, raw_text=False, relu_dropout=0.0, represent_length_by_ldpe=False, represent_length_by_lrpe=True, reset_lr_scheduler=False, reset_optimizer=False, restore_file='checkpoint_last.pt', save_dir='output', save_interval=1, save_interval_updates=0, seed=2723, sentence_avg=False, share_all_embeddings=True, share_decoder_input_output_embed=False, skip_invalid_size_inputs_valid_test=False, source_lang='wp_source', target_lang='wp_target', task='translation', train_subset='train', update_freq=[16], upsample_primary=1, valid_subset='valid', validate_interval=1, warmup_init_lr=1e-07, warmup_updates=4000, weight_decay=0.0)
-"""
 
-device_id = torch.device("cuda:0")
-seed = 2723
-distributed_world_size = 1
-distributed_rank = 0
-train_subset = 'train'
-
-
-def main():
-    parser = options.get_training_parser()
-    args = options.parse_args_and_arch(parser)
-
+def main(args):
+    if args.max_tokens is None:
+        args.max_tokens = 6000
     print(args)
 
     if not torch.cuda.is_available():
-        print('Training on CPU is not supported')
-        exit()
-
-    # Set pytorch to use the GPU
-    torch.cuda.set_device(device_id)
-
-    # Initialize weights with the seed used by the authors
-    torch.manual_seed(seed)
+        raise NotImplementedError('Training on CPU is not supported')
+    torch.cuda.set_device(args.device_id)
+    torch.manual_seed(args.seed)
 
     # Setup task, e.g., translation, language modeling, etc.
     task = tasks.setup_task(args)
 
     # Load dataset splits
-    load_dataset(task, ['train', 'valid'])
+    load_dataset_splits(task, ['train', 'valid'])
 
     # Build model and criterion
     model = task.build_model(args)
     criterion = task.build_criterion(args)
+    print('| model {}, criterion {}'.format(args.arch, criterion.__class__.__name__))
+    print('| num. model params: {}'.format(sum(p.numel() for p in model.parameters())))
 
     # Make a dummy batch to (i) warm the caching allocator and (ii) as a
     # placeholder DistributedDataParallel when there's an uneven number of
@@ -57,6 +43,11 @@ def main():
 
     # Build trainer
     trainer = Trainer(args, task, model, criterion, dummy_batch)
+    print('| training on {} GPUs'.format(args.distributed_world_size))
+    print('| max tokens per GPU = {} and max sentences per GPU = {}'.format(
+        args.max_tokens,
+        args.max_sentences,
+    ))
 
     # Initialize dataloader
     epoch_itr = task.get_batch_iterator(
@@ -71,13 +62,18 @@ def main():
         shard_id=args.distributed_rank,
     )
 
-    # Train
+    # Load the latest checkpoint if one is available
+    if not load_checkpoint(args, trainer, epoch_itr):
+        trainer.dummy_train_step([dummy_batch])
+
+    # Train until the learning rate gets too small
     max_epoch = args.max_epoch or math.inf
     max_update = args.max_update or math.inf
     lr = trainer.get_lr()
+    train_meter = StopwatchMeter()
+    train_meter.start()
     valid_losses = [None]
     valid_subsets = args.valid_subset.split(',')
-    # Max epoch, max updates or learning rate gets too small
     while lr > args.min_lr and epoch_itr.epoch < max_epoch and trainer.get_num_updates() < max_update:
         # train for one epoch
         train(args, trainer, task, epoch_itr)
@@ -88,9 +84,11 @@ def main():
         # only use first validation loss to update the learning rate
         lr = trainer.lr_step(epoch_itr.epoch, valid_losses[0])
 
-    # Save the model
-    save_model(args, trainer, epoch_itr, valid_losses[0])
-    print('Done.')
+        # save checkpoint
+        if epoch_itr.epoch % args.save_interval == 0:
+            save_checkpoint(args, trainer, epoch_itr, valid_losses[0])
+    train_meter.stop()
+    print('| done training in {:.1f} seconds'.format(train_meter.sum))
 
 
 def train(args, trainer, task, epoch_itr):
@@ -137,7 +135,7 @@ def train(args, trainer, task, epoch_itr):
         num_updates = trainer.get_num_updates()
         if args.save_interval_updates > 0 and num_updates % args.save_interval_updates == 0 and num_updates > 0:
             valid_losses = validate(args, trainer, task, epoch_itr, [first_valid])
-            save_model(args, trainer, epoch_itr, valid_losses[0])
+            save_checkpoint(args, trainer, epoch_itr, valid_losses[0])
 
         if num_updates >= max_update:
             break
@@ -155,6 +153,31 @@ def train(args, trainer, task, epoch_itr):
         meter = trainer.get_meter(k)
         if meter is not None:
             meter.reset()
+
+
+def get_training_stats(trainer):
+    stats = collections.OrderedDict()
+    stats['loss'] = '{:.3f}'.format(trainer.get_meter('train_loss').avg)
+    if trainer.get_meter('train_nll_loss').count > 0:
+        nll_loss = trainer.get_meter('train_nll_loss').avg
+        stats['nll_loss'] = '{:.3f}'.format(nll_loss)
+    else:
+        nll_loss = trainer.get_meter('train_loss').avg
+    stats['ppl'] = get_perplexity(nll_loss)
+    stats['wps'] = round(trainer.get_meter('wps').avg)
+    stats['ups'] = '{:.1f}'.format(trainer.get_meter('ups').avg)
+    stats['wpb'] = round(trainer.get_meter('wpb').avg)
+    stats['bsz'] = round(trainer.get_meter('bsz').avg)
+    stats['num_updates'] = trainer.get_num_updates()
+    stats['lr'] = trainer.get_lr()
+    stats['gnorm'] = '{:.3f}'.format(trainer.get_meter('gnorm').avg)
+    stats['clip'] = '{:.0%}'.format(trainer.get_meter('clip').avg)
+    stats['oom'] = trainer.get_meter('oom').avg
+    if trainer.get_meter('loss_scale') is not None:
+        stats['loss_scale'] = '{:.3f}'.format(trainer.get_meter('loss_scale').avg)
+    stats['wall'] = round(trainer.get_meter('wall').elapsed_time)
+    stats['train_wall'] = round(trainer.get_meter('train_wall').sum)
+    return stats
 
 
 def validate(args, trainer, task, epoch_itr, subsets):
@@ -207,31 +230,6 @@ def validate(args, trainer, task, epoch_itr, subsets):
     return valid_losses
 
 
-def get_training_stats(trainer):
-    stats = collections.OrderedDict()
-    stats['loss'] = '{:.3f}'.format(trainer.get_meter('train_loss').avg)
-    if trainer.get_meter('train_nll_loss').count > 0:
-        nll_loss = trainer.get_meter('train_nll_loss').avg
-        stats['nll_loss'] = '{:.3f}'.format(nll_loss)
-    else:
-        nll_loss = trainer.get_meter('train_loss').avg
-    stats['ppl'] = get_perplexity(nll_loss)
-    stats['wps'] = round(trainer.get_meter('wps').avg)
-    stats['ups'] = '{:.1f}'.format(trainer.get_meter('ups').avg)
-    stats['wpb'] = round(trainer.get_meter('wpb').avg)
-    stats['bsz'] = round(trainer.get_meter('bsz').avg)
-    stats['num_updates'] = trainer.get_num_updates()
-    stats['lr'] = trainer.get_lr()
-    stats['gnorm'] = '{:.3f}'.format(trainer.get_meter('gnorm').avg)
-    stats['clip'] = '{:.0%}'.format(trainer.get_meter('clip').avg)
-    stats['oom'] = trainer.get_meter('oom').avg
-    if trainer.get_meter('loss_scale') is not None:
-        stats['loss_scale'] = '{:.3f}'.format(trainer.get_meter('loss_scale').avg)
-    stats['wall'] = round(trainer.get_meter('wall').elapsed_time)
-    stats['train_wall'] = round(trainer.get_meter('train_wall').sum)
-    return stats
-
-
 def get_valid_stats(trainer):
     stats = collections.OrderedDict()
     stats['valid_loss'] = trainer.get_meter('valid_loss').avg
@@ -242,8 +240,8 @@ def get_valid_stats(trainer):
         nll_loss = trainer.get_meter('valid_loss').avg
     stats['valid_ppl'] = get_perplexity(nll_loss)
     stats['num_updates'] = trainer.get_num_updates()
-    if hasattr(save_model, 'best'):
-        stats['best'] = min(save_model.best, stats['valid_loss'])
+    if hasattr(save_checkpoint, 'best'):
+        stats['best'] = min(save_checkpoint.best, stats['valid_loss'])
     return stats
 
 
@@ -254,7 +252,7 @@ def get_perplexity(loss):
         return float('inf')
 
 
-def save_model(args, trainer, epoch_itr, val_loss):
+def save_checkpoint(args, trainer, epoch_itr, val_loss):
     if args.no_save or not distributed_utils.is_master(args):
         return
     epoch = epoch_itr.epoch
@@ -272,15 +270,15 @@ def save_model(args, trainer, epoch_itr, val_loss):
     )
     checkpoint_conds['checkpoint_best.pt'] = (
             val_loss is not None and
-            (not hasattr(save_model, 'best') or val_loss < save_model.best)
+            (not hasattr(save_checkpoint, 'best') or val_loss < save_checkpoint.best)
     )
     checkpoint_conds['checkpoint_last.pt'] = True  # keep this last so that it's a symlink
 
-    prev_best = getattr(save_model, 'best', val_loss)
+    prev_best = getattr(save_checkpoint, 'best', val_loss)
     if val_loss is not None:
-        save_model.best = min(val_loss, prev_best)
+        save_checkpoint.best = min(val_loss, prev_best)
     extra_state = {
-        'best': save_model.best,
+        'best': save_checkpoint.best,
         'train_iterator': epoch_itr.state_dict(),
         'val_loss': val_loss,
     }
@@ -297,7 +295,29 @@ def save_model(args, trainer, epoch_itr, val_loss):
             os.remove(old_chk)
 
 
-def load_dataset(task, splits):
+def load_checkpoint(args, trainer, epoch_itr):
+    """Load a checkpoint and replay dataloader to match."""
+    os.makedirs(args.save_dir, exist_ok=True)
+    checkpoint_path = os.path.join(args.save_dir, args.restore_file)
+    if os.path.isfile(checkpoint_path):
+        extra_state = trainer.load_checkpoint(checkpoint_path, args.reset_optimizer, args.reset_lr_scheduler,
+                                              eval(args.optimizer_overrides))
+        if extra_state is not None:
+            # replay train iterator to match checkpoint
+            epoch_itr.load_state_dict(extra_state['train_iterator'])
+
+            print('| loaded checkpoint {} (epoch {} @ {} updates)'.format(
+                checkpoint_path, epoch_itr.epoch, trainer.get_num_updates()))
+
+            trainer.lr_step(epoch_itr.epoch)
+            trainer.lr_step_update(trainer.get_num_updates())
+            if 'best' in extra_state:
+                save_checkpoint.best = extra_state['best']
+        return True
+    return False
+
+
+def load_dataset_splits(task, splits):
     for split in splits:
         if split == 'train':
             task.load_dataset(split, combine=True)
@@ -313,4 +333,16 @@ def load_dataset(task, splits):
 
 
 if __name__ == '__main__':
-    main()
+    parser = options.get_training_parser()
+    args = options.parse_args_and_arch(parser)
+
+    if args.distributed_port > 0 or args.distributed_init_method is not None:
+        from distributed_train import main as distributed_main
+
+        distributed_main(args)
+    elif args.distributed_world_size > 1:
+        from multiprocessing_train import main as multiprocessing_main
+
+        multiprocessing_main(args)
+    else:
+        main(args)
